168252dec94325509226fe9bafd8754979fb2054
Joren Sumagang <sumagangjoren@gmail.com>
2026-03-08 14:23:40 +0800

    refactor: update database connection to use DATABASE_URL and remove unused code

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,26 +1,8 @@
-// import mysql from 'mysql2'
 
-// console.log(process.env.DB_HOST)
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'spotify-chat'
-// })
-
-// export default db
 
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
-dotenv.config()
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+const db = mysql.createConnection(process.env.DATABASE_URL)
 
 export default db
 
--- spotify-chat.sql
+++ spotify-chat.sql
@@ -28,12 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `type` tinyint(1) NOT NULL DEFAULT 0,
   `message` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `deleted_at` datetime DEFAULT NULL
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -41,21 +42,15 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `user_id`, `type`, `message`, `created_at`, `deleted_at`) VALUES
-(1, 9, 0, 'yoo chat does she think of me', '2024-05-10 08:11:13', NULL),
-(2, 10, 0, 'ofc i think of u pookiee :333', '2024-05-10 08:11:30', NULL),
+(1, 9, 0, 'hi first message', '2024-05-10 08:11:13', NULL),
+(2, 10, 0, 'hello whats up', '2024-05-10 08:11:30', NULL),
 (3, 10, 1, '3GZD6HmiNUhxXYf8Gch723', '2024-05-10 08:11:39', NULL),
-(4, 9, 0, 'oa ampota', '2024-05-10 08:13:32', NULL),
+(4, 9, 0, 'ASDASDSADAS', '2024-05-10 08:13:32', NULL),
 (5, 9, 1, '7F6PtLP6fJPVtA1FWVkl8K', '2024-05-10 08:13:35', NULL),
-(6, 10, 0, 'ok lang wala naman ako kowinta : (', '2024-05-10 08:14:37', NULL),
-(7, 9, 0, 'HAHAHAHA ', '2024-05-10 08:14:52', NULL),
-(8, 10, 0, 'chaewon unsint a message.', '2024-05-10 08:15:18', NULL),
-(9, 10, 0, 'chaewon unsint a message.', '2024-05-10 08:15:19', NULL),
 (10, 11, 0, 'on skibidi this song goated frr', '2024-05-10 08:19:34', NULL),
 (11, 11, 1, '5GUYJTQap5F3RDQiCOJhrS', '2024-05-10 08:19:36', NULL),
 (12, 9, 1, '736PP5LTtREkDgktNmX3Gu', '2024-05-11 07:48:48', NULL),
 (13, 9, 0, 'AHAHAHAHAHA AHA AHAHAHAHAHAHA AHA AHAHAHAHAHAHA AHA AHAHAHAHAHAHA AHA AH', '2024-05-11 11:57:46', '2024-05-11 13:10:44'),
-(14, 9, 1, '1boXOL0ua7N2iCOUVI1p9F', '2024-05-11 12:00:32', NULL),
-(15, 9, 1, '7zFXmv6vqI4qOt4yGf3jYZ', '2024-05-11 12:13:25', NULL),
 (16, 9, 1, '2LlOeW5rVcvl3QcPNPcDus', '2024-05-11 12:13:34', NULL),
 (17, 12, 0, 'HAHAHA', '2024-05-11 12:31:31', NULL),
 (18, 12, 1, '6QqrxJe1iQxwLY6he6FLFo', '2024-05-11 12:31:40', NULL),
@@ -69,10 +64,11 @@ INSERT INTO `messages` (`id`, `user_id`, `type`, `message`, `created_at`, `delet
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -86,38 +82,7 @@ INSERT INTO `users` (`id`, `name`, `username`, `password`) VALUES
 (12, 'illit', 'illit', '$2b$10$xeg6fIdy7IwaS8fnS44nOuGp59OqrYTBwapCzo03CBwBRCcSnGQJO'),
 (13, 'sir ken', 'ken', '$2b$10$o6Y7vCckdClP.lZk2MyNhOse0WFBD7ZFLA.3fT48AJfA15/D9IeCG');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
